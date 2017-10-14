dd021ac055378e515edf85c0f8da01eac61073ca
Craig Richardson <craigargh@gmail.com>
2017-10-14 17:38:09 +0100

    Adjust styling

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import {Fretboard} from "./fretboard/fretboard";
 class App extends Component {
     render() {
         const positions = [
-            {finger: 0, fret: 2, guitar_string: 3},
+            {finger: 0, fret: 0, guitar_string: 1},
+            {finger: 1, fret: 1, guitar_string: 2},
+            {finger: 0, fret: 0, guitar_string: 3},
             {finger: 2, fret: 3, guitar_string: 4},
             {finger: 3, fret: 4, guitar_string: 5},
-            {finger: 3, fret: 5, guitar_string: 5},
-            {finger: 3, fret: 6, guitar_string: 5},
-            {finger: 3, fret: 7, guitar_string: 5},
-            {finger: 3, fret: 8, guitar_string: 5},
-            {finger: 3, fret: 9, guitar_string: 5},
         ];
 
         return (
             <div className="App">
-                <div style={{}}>
+                <div className='fretboards-container'>
+                    <Fretboard positions={positions}/>
+                    <Fretboard positions={positions}/>
+                    <Fretboard positions={positions}/>
                     <Fretboard positions={positions}/>
                 </div>
             </div>
