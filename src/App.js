70dc53acc3e989b2e77beb2f063fb6457e05f60f
Craig Richardson <craigargh@gmail.com>
2017-10-14 09:33:45 +0100

    Correct position css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Position} from "./fretboard/position";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <div className="circle fret-position-one">
-                        <div className="position-text">1</div>
-                    </div>
-                    <div className="circle fret-position-two">
-                        <div className="position-text">2</div>
-                    </div>
+                    <Position finger={1}/>
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
--- src/fretboard/position.jsx
+++ src/fretboard/position.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './position.css'
 
 export class Position extends React.Component {
     static propTypes = {
