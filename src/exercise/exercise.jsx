10435df274ba2993e86b360b70ab0587cfdd1f9a
Craig Richardson <craigargh@gmail.com>
2017-10-15 09:42:56 +0100

    Add tablature and fretboards to exercise

--- src/exercise/exercise.jsx
+++ src/exercise/exercise.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import {Tablature} from "../tablature/tablature";
+import {Fretboard} from "../fretboard/fretboard";
 import './exercise.css';
 
 export class Exercise extends React.Component {
@@ -9,7 +11,33 @@ export class Exercise extends React.Component {
         shapes: PropTypes.array,
     };
 
-    render() {
-        return <div/>
+    static defaultProps = {
+        shapes: [],
+        sequence: [],
     };
+
+    render() {
+        const fretboards = this.makeFretboards();
+        const tablature = this.makeTablature();
+
+        return <div>
+            {fretboards}
+            {tablature}
+        </div>
+    };
+
+    makeTablature() {
+        const {sequence} = this.props;
+        return <Tablature sequence={sequence}/>;
+    }
+
+    makeFretboards() {
+        const {shapes} = this.props;
+
+        return shapes.map((shape, index) => {
+            const key = `fretboard-${index}`;
+
+            return <Fretboard {...shape} key={key}/>
+        });
+    }
 }
--- src/test/exercise.test.jsx
+++ src/test/exercise.test.jsx
@@ -7,7 +7,51 @@ import {Exercise} from "../exercise/exercise";
 Enzyme.configure({adapter: new Adapter()});
 
 describe('Exercise component', () => {
-    it('should do something', () => {
+    it('should render a tablature component', () => {
+        const wrapper = shallow(<Exercise/>);
+        const tablature = wrapper.find('Tablature');
 
+        expect(tablature.length).toBe(1);
+    });
+
+    it('should pass the sequence to the tablature component', () => {
+        const sequence = [
+            {fret: 1, guitar_string: 1, order: 1},
+            {fret: 2, guitar_string: 2, order: 2},
+            {fret: 3, guitar_string: 3, order: 3},
+        ];
+
+        const wrapper = shallow(<Exercise sequence={sequence}/>);
+        const tablature = wrapper.find('Tablature').first();
+
+        expect(tablature.props().sequence).toBe(sequence);
+    });
+
+    it('should render a fretboard for every shape', () => {
+        const shapes = [
+            {positions: [1, 2, 3]},
+            {positions: [4, 5, 6]},
+            {positions: [7, 8, 9]},
+        ];
+
+        const wrapper = shallow(<Exercise shapes={shapes}/>);
+        const fretboards = wrapper.find('Fretboard');
+
+        expect(fretboards.length).toEqual(3);
+    });
+
+    it('should pass the positions to each fretboard', () => {
+        const shapes = [
+            {positions: [1, 2, 3]},
+            {positions: [4, 5, 6]},
+            {positions: [7, 8, 9]},
+        ];
+
+        const wrapper = shallow(<Exercise shapes={shapes}/>);
+        const fretboards = wrapper.find('Fretboard');
+
+        expect(fretboards.at(0).props().positions).toEqual(shapes[0].positions);
+        expect(fretboards.at(1).props().positions).toEqual(shapes[1].positions);
+        expect(fretboards.at(2).props().positions).toEqual(shapes[2].positions);
     });
 });
